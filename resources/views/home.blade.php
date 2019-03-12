4e1fadf00f8d45b5d5e0175d97283fb5da13daf0
Jahn <jorgechido75@hotmail.com>
2019-03-12 10:47:29 -0600

    Se termina la vista de home thoughts min 5:37

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">¿En que estás pensando ahora?</div>
+                <div class="card-header">¿En que estás pensando ahora?
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,12 +15,34 @@
                         </div>
                     @endif
 
-                    <form action="form-group">
-                        <input type="text" class="form-control" name="">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="thought">Ahora estoy pensando en:</label>
+                            <input type="text" class="form-control" name="thought">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            Enviar pensamiento
+                        </button>                      
                     </form>
                 </div>
             </div>
+
+            <div class="card" style="margin-top: 20px">
+                <div class="card-header">Publicado en 12/2019
+                </div>
+                <div class="card-body">
+                    <p>mucho texto</p>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-default">
+                        Editar
+                    </button>
+                    <button class="btn btn-danger">
+                        Eliminar
+                    </button>
+                </div>              
+            </div>
         </div>
-    </div>
+    </div>   
 </div>
 @endsection
