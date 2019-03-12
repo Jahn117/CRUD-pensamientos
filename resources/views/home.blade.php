4b075800cb91fdcd464fbe49221728e139b30275
Jahn <jorgechido75@hotmail.com>
2019-03-12 09:56:46 -0600

    avance al minuto 4:12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">¿En que estás pensando ahora?</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form action="form-group">
+                        <input type="text" class="form-control" name="">
+                    </form>
                 </div>
             </div>
         </div>
